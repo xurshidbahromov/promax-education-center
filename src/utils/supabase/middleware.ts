22ec41f558bdecccf338e63905f9ec3d5ab123b6
Xurshid <xurshidbahromov06@gmail.com>
2026-06-19 13:55:02 +0500

    fix: redirect unauthenticated users only when accessing protected routes

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -35,14 +35,9 @@ export async function updateSession(request: NextRequest) {
  data: { user },
  } = await supabase.auth.getUser()
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/tg') &&
-    !request.nextUrl.pathname.startsWith('/api/telegram') &&
-    !request.nextUrl.pathname.startsWith('/register')
-  ) {
+  const isProtectedRoute = request.nextUrl.pathname.startsWith('/dashboard') || request.nextUrl.pathname.startsWith('/admin')
+
+  if (!user && isProtectedRoute) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/login'
